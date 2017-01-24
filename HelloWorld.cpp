4b99e7251231da7838e27f2b701eb12871d88c50
Joo10 <jooyul7013@naver.com>
2017-01-24 20:15:49 +0900

    FirstStep

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+	int year=2017;
+	std::cout<<"Hello World"<<std::endl;
+	std::cout<<"Hello"<<" "<<"World"<<std::endl;
+	std::cout<<year<<"-"<<"01-24"<<std::endl;
+	
+	return 0;
+}
